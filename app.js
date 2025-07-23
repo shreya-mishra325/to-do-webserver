9eac345fd9324d7a3ff72ff4002d42249d42a7eb
Shreya Mishra <shreyamishra2k05@gmail.com>
2025-07-23 23:33:44 +0530

    server created

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+let todos = [];
+let id = 1;
+
+app.get('/', (req,res)=>{
+    console.log(req.method);
+    res.send(`server started`);
+});
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req,res) => {
+    const {task} = req.body;
+    if(!task || typeof task !== 'string')
+        return res.status(400).json({error: 'task not written as a string.'});
+
+const newItem = {
+    id: id++,
+    task: task.trim(),
+}
+todos.push(newItem);
+res.status(201).json(newItem);
+});
+
+app.delete('/todos/:id', (req,res) => {
+    const id1 = Number(req.params.id);
+    const index = todos.findIndex(item => item.id === id1);
+    if(index === -1) {
+        return res.status(404).json({error: `to-do not found with id ${id1}`});
+    }
+
+const deleted = todos.splice(index, 1);
+res.json({message: `deleted successfully`, deleted});
+});
+
+app.listen(3000, () => {
+    console.log(`server running at port 3000`);
+});
